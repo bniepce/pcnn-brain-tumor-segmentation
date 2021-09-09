f5ff21c2b005d7ddd7cae54066f29d5a88f88329
Brad Niepceron <87471823+bniepce@users.noreply.github.com>
2021-09-09 19:45:16 +0200

    Update run_optimization.py

--- run_optimization.py
+++ run_optimization.py
@@ -1,15 +1,21 @@
 from models import *
 from utils.data import H5Dataset
 from optimization import DE
+import argparse
 
 if __name__ == "__main__":
 
     np.random.seed(1234)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_path', type=str, required=True)
+    args = parser.parse_args()
 
-    model = UnitLinkingPCNN
+    models = [UnitLinkingPCNN, FLSCM, StandardPCNN]
     de = DE(model)
     
-    dataset = H5Dataset('mri_data.h5')
+    dataset = H5Dataset(args.dataset_path)
     X, Y = dataset[0][0], dataset[0][1]
-    opt_params = de.run((X, Y))
-    print(opt_params)
+    for i in models:
+        opt_params = de.run((X, Y))
+        print(i.__name__)
+        print(opt_params)
